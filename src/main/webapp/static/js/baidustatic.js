40efc567b0ad3a1741b1ef92abe9ca58983f87b0
mnuo <yu11115@163.com>
2016-06-06 19:01:31 +0800

    add baidu statistic

---
+++ src/main/webapp/static/js/baidustatic.js
@@ -0,0 +1,8 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?9202a7b43d0e0fb479787dccf0f00360";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
